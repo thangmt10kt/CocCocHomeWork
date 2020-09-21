93b8b00a5157e379df1a0e8e229dffc7631ec7cc
thangmt10kt <71555490+thangmt10kt@users.noreply.github.com>
2020-09-21 21:39:36 +0700

    Cover bad_alloc error

--- main.cpp
+++ main.cpp
@@ -4,43 +4,73 @@
 #include <fstream>
 #include <vector>
 #include <cstdlib>
-#include <algorithm>  
-#include <climits> 
+#include <algorithm>   
+#include <climits>
+#include <iostream>
 
 using namespace std;
 
-/* Refill merge buffer */
-void fileMergeBuff(fstream &file, vector<string> &vecStrs, string &pend, long long int memLimit)
+void createChunkFile(int chunkIdx, vector<string> &strs)
 {
-	vector<string> dataBuff;
+	char buffNum[33];
+	itoa(chunkIdx, buffNum, 10);
+	fstream chunkFile;
+	
+	chunkFile.open(buffNum, ios::out);
+	sort(strs.begin(),strs.end());
+	/* Write string to chunk file */
+	for(string str:strs)
+	{
+		chunkFile << str << endl;
+	}
+	chunkFile.close();
+}
+
+/* Refill merge buffer */
+inline void fileMergeBuff(fstream &file, vector<string> &vecStrs, string &pend, long long int memLimit)
+{
 	string lineData;
 	long long int usedMem = 0;
+	char stopFlag = 0;
+	
+	vecStrs.clear();
 	
 	/* In case a line is not put to merge buffer */
 	if(pend.size() != 0)
 	{
-		dataBuff.push_back(pend);
+		vecStrs.push_back(pend);
 		usedMem = pend.size();
 	}
-	
+
 	pend.clear();
-	
+
 	/* Update the merge buffer */
 	while(getline(file, lineData))
 	{
 		if((usedMem + lineData.size()) <= memLimit)
 		{
-			usedMem += lineData.size() + sizeof(string);
-			dataBuff.push_back(lineData);
+			try
+			{
+				vecStrs.push_back(lineData);
+				usedMem = usedMem + lineData.size() + sizeof(string);
+			}
+			catch (std::bad_alloc& ba)
+			{
+				pend = lineData;
+				stopFlag = 1;
+			}
 		}
 		else
 		{
 			pend = lineData;
+			stopFlag = 1;
+		}
+		
+		if(stopFlag == 1)
+		{
 			break;
 		}
 	}
-	
-	vecStrs = dataBuff;
 }
 
 
@@ -51,66 +81,57 @@ int main(int argc, char **argv)
 	char *outputFileName = argv[2];
 	fstream ipFile;
 	fstream opFile;
-	int usedSize = 0;
+	long long int usedSize = 0;
 	int cntChunk = 0;
 	char buffNum[33];
-	
+
 	/* get the memory size */
 	sscanf(argv[3], "%lld", &availMemSize);
 
-	/* Only use 1/2 available memory to ensure the health of software */
-	if(availMemSize >= LONG_MAX)
-	{
-		availMemSize = LONG_MAX;
-	}	
-	availMemSize = availMemSize / 2;
-	
+	/* Only use 3/2 available memory to ensure the health of software */
+	availMemSize = availMemSize*3/4;
+
 	/* Distribute input file to chunk files */
 	{
-		fstream chunkFile;
 		vector<std::string> lineStr;
 		std::string tp;
 		
 		ipFile.open(inputFileName, ios::in);
-			
+
 		while(getline(ipFile, tp))
 		{
 			/* Get string from input file then sort them*/
 			if((usedSize + tp.size()) <= availMemSize)
 			{
-				lineStr.push_back(tp);
-				usedSize += tp.size() + sizeof(string);
+				try{
+					lineStr.push_back(tp);
+					usedSize += tp.size() + sizeof(string);
+				}
+				catch(std::bad_alloc& ba)
+				{
+					/* Create new chunk file in case the allocation is fail */
+					cntChunk++;
+					createChunkFile(cntChunk, lineStr);
+					lineStr.clear();
+					lineStr.push_back(tp);
+					usedSize = tp.size() + sizeof(string);
+				}
 			}
 			else{
-				cntChunk++;
-				itoa(cntChunk, buffNum, 10);
-				chunkFile.open(buffNum, ios::out);
-				sort(lineStr.begin(),lineStr.end());
-				/* Write string to chunk file */
-				for(string str:lineStr)
-				{
-					chunkFile << str << endl;
-				}
-				chunkFile.close();
-				lineStr.clear();
-				lineStr.push_back(tp);
-				usedSize = tp.size() + sizeof(string);
+					cntChunk++;
+					createChunkFile(cntChunk, lineStr);
+					lineStr.clear();
+					lineStr.push_back(tp);
+					usedSize = tp.size() + sizeof(string);
 			}
 		}
 		ipFile.close();
-		
+
 		/* Write remaining string to chunk file */
 		if(lineStr.size() != 0)
 		{
 			cntChunk++;
-			itoa(cntChunk, buffNum, 10);
-			chunkFile.open(buffNum, ios::out);
-			sort(lineStr.begin(),lineStr.end());
-			for(string str:lineStr)
-			{
-				chunkFile << str << endl;
-			}
-			chunkFile.close();
+			createChunkFile(cntChunk, lineStr);
 			lineStr.clear();
 			usedSize = 0;
 		}
@@ -125,26 +146,26 @@ int main(int argc, char **argv)
 	char firstLineFlag = 0;
 	long long int sortBufSize = 0;
 	long long int chunkSize = availMemSize / (cntChunk + 1);
-	
+
 	/* Clear old content */
 	opFile.open(outputFileName, ios::out|ios::trunc);
 	opFile.close();
-	
+
 	opFile.open(outputFileName, ios::app);
-	
+
 	/* Open all chunk file */
 	for(int idx = 0; idx < cntChunk; ++idx)
 	{
 		itoa(idx + 1, buffNum, 10);
 		chunkFiles[idx].open(buffNum, ios::in);
 	}
-	
+
 	/* Load data from chunk files to merge buffers */ 
-	for(int idx = 0; idx < chunkFiles.size(); ++idx)
+	for(int idx = 0; idx < cntChunk; ++idx)
 	{
 		fileMergeBuff(chunkFiles[idx],multiMergWay[idx], pendingStr[idx], chunkSize);
 	}
-
+	
 	/* Merge all buffer */
 	while(chunkFiles.size() > 0)
 	{
@@ -159,12 +180,27 @@ int main(int argc, char **argv)
 				smallestStr = multiMergWay[idx][checkingPoint[idx]];
 			}
 		}
-		
+
 		/* Add the smallest string to sort buffer which shall be wrote to output file when it is full */
 		if(sortBufSize + smallestStr.size() <= chunkSize)
 		{
-			sortBuf.push_back(smallestStr);
-			sortBufSize += smallestStr.size();
+			try
+			{
+				sortBuf.push_back(smallestStr);
+				sortBufSize += smallestStr.size() + sizeof(string);
+			}
+			catch(std::bad_alloc& ba)
+			{
+				/* update the output file in case the allocation is fail */
+				for(string str:sortBuf)
+				{
+					opFile << str << endl;
+				}
+
+				sortBuf.clear();
+				sortBufSize = smallestStr.size() + sizeof(string);
+				sortBuf.push_back(smallestStr);
+			}
 		}
 		else
 		{
@@ -173,12 +209,12 @@ int main(int argc, char **argv)
 			{
 				opFile << str << endl;
 			}
-			
+
 			sortBuf.clear();
+			sortBufSize = smallestStr.size() + sizeof(string);
 			sortBuf.push_back(smallestStr);
-			sortBufSize = smallestStr.size();
 		}
-		
+
 		/* refill merge buffer in case it is empty */
 		if(multiMergWay[smlIdx].size() == checkingPoint[smlIdx] + 1)
 		{
@@ -189,7 +225,7 @@ int main(int argc, char **argv)
 		{
 			checkingPoint[smlIdx]++;
 		}
-		
+
 		/* Clear empty chunk buffer */
 		if(multiMergWay[smlIdx].size() == 0)
 		{
@@ -200,15 +236,18 @@ int main(int argc, char **argv)
 			pendingStr.erase(pendingStr.begin() + smlIdx);
 		}
 	}
-	
-	/* Add remaining string */
-	for(int idx = 0; idx < sortBuf.size() - 1; ++idx)
+
+	if(sortBuf.size() > 0)
 	{
-		opFile << sortBuf[idx] << endl;
+		/* Add remaining string */
+		for(int idx = 0; idx < sortBuf.size() - 1; ++idx)
+		{
+			opFile << sortBuf[idx] << endl;
+		}
+
+		opFile << sortBuf[sortBuf.size() - 1];
 	}
-	
-	opFile << sortBuf[sortBuf.size() - 1];
 	
 	opFile.close();
 	return 0;
-}
+} 
